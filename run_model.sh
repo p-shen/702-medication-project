aff7ea73e2733783b75017b3422bb772ef86e0ae
Peter Shen <peterzhuo.shen@gmail.com>
2018-04-19 18:58:02 -0400

    change run parameters for cluster

--- run_model.sh
+++ run_model.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #SBATCH -p short   # queue name
-#SBATCH -t 0-8:00       # hours:minutes runlimit after which job will be killed.
-#SBATCH -n 12      # number of cores requested
-#SBATCH --mem=32G # memory requested
+#SBATCH -t 0-2:00       # hours:minutes runlimit after which job will be killed.
+#SBATCH -n 8      # number of cores requested
+#SBATCH --mem=12G # memory requested
 #SBATCH -J modeling         # Job name
 #SBATCH -o %j.out       # File to which standard out will be written
 #SBATCH -e %j.err       # File to which standard err will be written
